47ef9d6a0bac30070cb7b713d1cec0e0c0e6a139
jamfritz <jamfritz>
2023-11-14 11:30:36 -0500

    small changes

--- app/src/main/java/com/example/project9/SelfieFragment.kt
+++ app/src/main/java/com/example/project9/SelfieFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import com.example.project9.databinding.FragmentSelfieBinding
 
@@ -19,9 +21,17 @@ class SelfieFragment: Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentSelfieBinding.inflate(inflater, container, false)
+        val viewModelAccel = binding.viewModelAccel
         val view = binding.root
         val url = SelfieFragmentArgs.fromBundle(requireArguments())
         Glide.with(requireContext()).load(url.url).into(binding.ivSelfie)
+
+        viewModelAccel.shakeEvent.observe(viewLifecycleOwner, Observer { isShaken ->
+            if (isShaken) {
+                viewModelAccel.shakeEvent.value = false
+                view.findNavController().navigate(R.id.action_selfies_to_picture)
+            }
+        })
         return view
     }
 }
