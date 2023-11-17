0f22658940f45b254cfe7cac3479733037eeeab3
Ashley Steitz <89358454+asteitz@users.noreply.github.com>
2023-11-16 23:06:15 -0500

    Update SelfieFragment.kt

--- app/src/main/java/com/example/project9/SelfieFragment.kt
+++ app/src/main/java/com/example/project9/SelfieFragment.kt
@@ -14,6 +14,9 @@ import com.bumptech.glide.Glide
 import com.example.project9.databinding.FragmentSelfieBinding
 
 class SelfieFragment: Fragment() {
+    /**
+     * Fragment responsible for displaying a single selfie along with accelerometer shake detection.
+     */
 
     val TAG = "PostsFragment"
     private var _binding: FragmentSelfieBinding? = null
@@ -24,20 +27,35 @@ class SelfieFragment: Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        /**
+         * Called to have the fragment instantiate its user interface view.
+         *
+         * @param inflater The LayoutInflater object that can be used to inflate views.
+         * @param container If non-null, this is the parent view that the fragment's UI should be attached to.
+         * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state.
+         * @return The created view or null.
+         */
         // Inflate the layout for this fragment
         _binding = FragmentSelfieBinding.inflate(inflater, container, false)
         sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val view = binding.root
+
+        // Obtain the shared ViewModel for accelerometer data
         val viewModelAccel: SensorsViewModel by activityViewModels()
         binding.viewModelAccel = viewModelAccel
         binding.lifecycleOwner = viewLifecycleOwner
+
+        // Load the selfie image from the arguments using Glide
         val url = SelfieFragmentArgs.fromBundle(requireArguments())
         Glide.with(requireContext()).load(url.url).into(binding.ivSelfie)
 
+        // Initialize accelerometer sensor and observe shake events
         viewModelAccel.initializeAccel(Accelerometer(this.requireContext()))
         viewModelAccel.shakeEvent.observe(viewLifecycleOwner) { isShaken ->
             if (isShaken) {
                 viewModelAccel.shakeEvent.value = false
+
+                // Navigate to the "takePicture" destination on a shake event
                 val navController = view.findNavController()
                 if (navController.currentDestination?.id != R.id.takePicture) {
                     navController.navigate(R.id.action_selfie_to_picture)
