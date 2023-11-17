fc6507e0af9f037942782782908fa669e0a47d34
Ashley Steitz <89358454+asteitz@users.noreply.github.com>
2023-11-16 23:05:45 -0500

    Update MeasurableSensor.kt

--- app/src/main/java/com/example/project9/MeasurableSensor.kt
+++ app/src/main/java/com/example/project9/MeasurableSensor.kt
@@ -3,7 +3,12 @@ package com.example.project9
 abstract class MeasurableSensor(
     protected val sensorType: Int
 ) {
-
+    /**
+     * An abstract class representing a measurable sensor.
+     * Provides common functionality for sensors that measure numerical values.
+     *
+     * @param sensorType The type of the sensor as an integer.
+     */
     protected var onSensorValuesChanged: ((List<Float>) -> Unit)? = null
 
     abstract val doesSensorExist: Boolean
@@ -12,6 +17,11 @@ abstract class MeasurableSensor(
     abstract fun stopListening()
 
     fun setOnSensorValuesChangedListener(listener: (List<Float>) -> Unit) {
+        /**
+         * Sets the listener to be called when sensor values change.
+         *
+         * @param listener The listener function to be called with a list of new sensor values.
+         */
         onSensorValuesChanged = listener
     }
 }
